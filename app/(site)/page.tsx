e7610e7b1e0cafb137600345b6d3185c0701f8f3
angelDova <angelfcb01@yahoo.com>
2023-05-26 23:55:30 -0700

    replaced logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { FaJediOrder } from "react-icons/fa";
 
 import AuthForm from "./components/AuthForm";
 
@@ -9,13 +10,7 @@ export default function Home() {
     py-12 sm:px-6 lg:px-8 bg-gray-100"
     >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
-          alt="Logo"
-          height="48"
-          width="48"
-          className="mx-auto w-auto"
-          src="/images/logo.png"
-        />
+        <FaJediOrder size={100} className="mx-auto w-auto" />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
